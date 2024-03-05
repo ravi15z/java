bfe54d9dc5909b43f78510e656edd9b525253b6e
ravi15z <rv.gaikwad15@gmail.com>
2024-03-05 15:30:05 +0530

    Initial commit

---
+++ src/main/java/com/java/java_git/JavaGitApplication.java
@@ -0,0 +1,13 @@
+package com.java.java_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/java_git/JavaGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.java_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
